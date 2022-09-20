e22f4a428d7feaf2e6507d77ca4afc0f73dcf177
SuhilovVitaly <98092169+SuhilovVitaly@users.noreply.github.com>
2022-09-20 20:01:47 +0300

    Create startap for tests

---
+++ OutlandSpace.Tests/Startup.cs
@@ -0,0 +1,23 @@
+﻿using log4net.Config;
+using NUnit.Framework;
+
+namespace OutlandSpace.Tests
+{
+    [SetUpFixture]
+    public class Startup
+    {
+        [OneTimeSetUp]
+        public void RunBeforeAnyTests()
+        {
+            XmlConfigurator.Configure();
+
+            //BasicConfigurator.Configure();
+
+            //log4net.Config.BasicConfigurator.Configure(
+            //    new log4net.Appender.ConsoleAppender
+            //    {
+            //        Layout = new log4net.Layout.SimpleLayout()
+            //    });
+        }
+    }
+}
