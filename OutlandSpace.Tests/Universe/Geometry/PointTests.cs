dd56a39409704204b119f0212e365f3d1d90a72d
SuhilovVitaly <98092169+SuhilovVitaly@users.noreply.github.com>
2023-01-04 10:40:54 +0200

    Refactor universe point equals methods

---
+++ OutlandSpace.Tests/Universe/Geometry/PointTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using OutlandSpace.Universe.Geometry;
+
+namespace OutlandSpace.Tests.Universe.Geometry
+{
+    [TestFixture]
+    public class PointTests
+    {
+        [Test]
+        public void ConvertToTurnSnapshotShouldBeCorrect()
+        {
+            Assert.IsTrue(new Point(100.0f, 100.0f).Equals(new Point(100.0f, 100.0f)));
+            Assert.IsTrue(new Point(100.0f, 100.0f) == new Point(100.0f, 100.0f));
+
+            Assert.IsFalse(new Point(100.0f, 100.0f).Equals(new Point(100.0f, 100.01f)));
+            Assert.IsFalse(new Point(100.0f, 100.0f) == new Point(100.0f, 100.01f));
+        }
+    }
+}
